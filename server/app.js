6974938f1d00a7faf9e45c3de5a10a56d4fd4844
Marcoandres <marcoandresha@gmail.com>
2024-11-02 08:05:26 -0500

    cambioo de todooo

--- server/app.js
+++ server/app.js
@@ -17,10 +17,12 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 app.get('/', (req, res) => {
