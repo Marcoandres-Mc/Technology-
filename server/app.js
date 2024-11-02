0bfe98c7b38509cdc93a0febce57839f8486d58a
Marcoandres <marcoandresha@gmail.com>
2024-11-02 07:43:28 -0500

    cambioo de todooo

--- server/app.js
+++ server/app.js
@@ -6,12 +6,17 @@ import userProducts from './routes/product.routes.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 dotenv.config();
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
